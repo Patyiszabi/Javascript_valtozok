bc08433dbcc9c33b73adbef90998d838e4044b90
Patyiszabi <patyi.szabolcs@diak.szbi-pg.hu>
2025-02-04 09:31:01 +0100

    .

---
+++ valtozok.js
@@ -0,0 +1,26 @@
+// valtozo deklaralasa hasznalat elott elavult modszererrel
+var szoveg = "Ez egy szöveg";
+console.log(szoveg);
+
+// valtozo ertekenek modositasa
+let szoveg2 = "Ez egy szöveg";
+console.log(szoveg2);
+szoveg2 = "Ez egy más szöveg";
+console.log(szoveg2);
+
+const szoveg3 = "Ez egy szöveg";
+//szoveg3 = "Ez egy más szöveg";  HIBA!
+console.log(szoveg3);
+
+// valtozo ujradefinialasa
+var x = 5; 
+console.log(x); 
+var x = "alma"; 
+console.log(x); 
+
+let y = 5; 
+console.log(y); 
+/*let y = "alma"; 
+console.log(y);  // HIBA!
+*/
+
---
+++ valtozok2.js
@@ -0,0 +1,10 @@
+let a = 2;
+console.log('a = ' + a + ' típusa ' + typeof a);
+
+const pi = 3.14;
+console.log('pi = ' + pi + ' típusa ' + typeof pi);
+
+//osszeadas
+console.log(0.1+0.2);
+//osszeadas kerekitese
+console.log((0.1+0.2).toFixed(2));
