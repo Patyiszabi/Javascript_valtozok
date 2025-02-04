116253580dbd234c1a52e782c120a4eeb808da2d
Patyiszabi <patyi.szabolcs@diak.szbi-pg.hu>
2025-02-04 10:32:30 +0100

    Update valtozok2.js

--- valtozok2.js
+++ valtozok2.js
@@ -8,3 +8,36 @@ console.log('pi = ' + pi + ' típusa ' + typeof pi);
 console.log(0.1+0.2);
 //osszeadas kerekitese
 console.log((0.1+0.2).toFixed(2));
+
+
+console.log(1/0); // Infinity
+console.log(-1/0); // -Infinity
+
+
+let firstname = "John";
+let lastname = "Doe";
+
+console.log(`Hello, ${firstname} ${lastname}`);
+
+//objektumok
+let person = {
+    firstname: "John",
+    lastname: "Doe",
+    age: 40,
+    isStudent: false
+};
+
+console.log(person);
+console.log(person.firstname);
+console.log(person.lastname);
+console.log(person.age);
+console.log(person.isStudent);
+console.log(person.address); // undefined
+
+// boolean
+console.log(typeof true);
+console.log(Boolean(0));
+console.log(Boolean("a"));
+console.log(Boolean(1));
+
+
